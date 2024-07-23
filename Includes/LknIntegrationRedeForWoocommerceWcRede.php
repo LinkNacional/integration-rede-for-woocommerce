d4559f10ba0490b7c92282aad7e3508dc83f21e9
Emanuel Lopes <lopes.emanuel@linknacional.com>
2024-07-23 16:09:55 +0000

    chore: Update version to 3.1.2

--- Includes/LknIntegrationRedeForWoocommerceWcRede.php
+++ Includes/LknIntegrationRedeForWoocommerceWcRede.php
@@ -7,7 +7,7 @@ use WP_Query;
 final class LknIntegrationRedeForWoocommerceWcRede {
     public const FILE = __FILE__;
 
-    public const VERSION = '3.1.1';
+    public const VERSION = '3.1.2';
     protected static $instance = null;
 
     public function __construct() {
