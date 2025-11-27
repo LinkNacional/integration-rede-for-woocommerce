4b8bb848fde5dbfb1c74a9651cf7e3b5d77c6e98
Davi <contato@linknacional.com.br>
2025-11-27 11:22:58 -0300

    Update download link for fraud detection plugin
    Redirecionando o download para o plugin no marketplace do WordPress.

--- Includes/views/notices/lkn-integration-rede-for-woocommerce-notice-download.php
+++ Includes/views/notices/lkn-integration-rede-for-woocommerce-notice-download.php
@@ -4,5 +4,5 @@
  */
 ?>
 <div class="notice notice-warning is-dismissible">
-    <p><strong>Recomendação:</strong> Instale o plugin gratuito de prevenção contra fraudes para WooCommerce e aumente a segurança dos seus pedidos. <a href="https://github.com/LinkNacional/fraud-detection-for-woocommerce/releases" target="_blank">Download</a></p>
+    <p><strong>Recomendação:</strong> Instale o plugin gratuito de prevenção contra fraudes para WooCommerce e aumente a segurança dos seus pedidos. <a href="https://wordpress.org/plugins/fraud-and-scam-detection-for-woocommerce/" target="_blank">Download</a></p>
 </div>
